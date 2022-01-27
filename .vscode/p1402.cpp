09aa91b7fa245ca27ce56ca91f969e82d9c80f97
Venarys <1098477336@qq.com>
2022-01-27 15:55:18 +0800

    p1402_1

--- .vscode/p1402.cpp
+++ .vscode/p1402.cpp
@@ -1,9 +1,10 @@
 #include <cstdio>
 #include <cstring>
 using namespace std;
-int w1[6000],l1[6000],w2[4000],l2[4000],j1,j2,i;
+int w1[6000],l1[6000],w2[4000],l2[4000];
 int main()
 {
+    int j1=0,j2=0,i=0;
     for(i = 0;getchar() != 'E';i++){
             if(getchar() == 'W'){
                 w1[j1]++;
@@ -17,12 +18,11 @@ int main()
                 j1++;
             if((w2[j2]>21 || l2[j2]>21) && (w2[j2]-l2[j2] > 1))
                 j2++;
-        }
     }
 
     for(int m = 0;m <= j1;m++)
         printf("%d:%d\n",w1[m],l1[m]);
-    puts("");
+    printf("\n");
     for(int m = 0;m <= j2;m++)
         printf("%d:%d\n",w2[m],l2[m]);
  
