efd9f12045cb3ac47f1ecae16ac5f7f9c58d2380
Venarys <1098477336@qq.com>
2022-01-27 15:47:53 +0800

    p1402

---
+++ .vscode/p1402.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int w1[6000],l1[6000],w2[4000],l2[4000],j1,j2,i;
+int main()
+{
+    for(i = 0;getchar() != 'E';i++){
+            if(getchar() == 'W'){
+                w1[j1]++;
+                w2[j2]++;
+            }
+            else if(getchar() == 'L'){ 
+                l1[j1]++;
+                l2[j2]++;
+            }
+            if((w1[j1]>11 || l1[j1]>11) && (w1[j1]-l1[j1] > 1))
+                j1++;
+            if((w2[j2]>21 || l2[j2]>21) && (w2[j2]-l2[j2] > 1))
+                j2++;
+        }
+    }
+
+    for(int m = 0;m <= j1;m++)
+        printf("%d:%d\n",w1[m],l1[m]);
+    puts("");
+    for(int m = 0;m <= j2;m++)
+        printf("%d:%d\n",w2[m],l2[m]);
+ 
+    return 0;
+}
