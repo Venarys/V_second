eebcbdd5bb07f19a001c7e7067d85193ce501c9f
Venarys <1098477336@qq.com>
2022-01-27 14:26:12 +0800

    p1423

--- .vscode/p1423.cpp
+++ .vscode/p1423.cpp
@@ -2,10 +2,12 @@
 int main()
 {
     int step;
-    float dist=0,d0=2;
+    float dist=0,dt=2,d0=0;
     scanf("%f",&dist);
-    for(step = 1;d0 < dist;step++)
-        d0 += 2*0.98;
-    printf("%d\n",step);
+    for(step = 1;d0 < dist;step++){
+        d0 += dt;
+        dt *= 0.98;
+    }
+    printf("%d\n",--step);
     return 0;
 }
